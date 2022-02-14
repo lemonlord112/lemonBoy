d2d18996cf2e2c9bfef489326b4c6e8e43a54a8f
lemonlord112 <92231328+lemonlord112@users.noreply.github.com>
2022-02-14 18:02:18 +1300

    Add files via upload

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+  
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 
@@ -65,13 +66,10 @@ const textNodes = [
   {
     id: 1,
     text: `Hello world! my name is Lemon Boy. This is my first ever JavaScript game!
-
     This is a STORY ADVENTURE GAME, if you're not in the mood for reading, don't play. This is a game about me, Lemon Boy, and how I navigate my life in a chronically burdening society. I hope you can help me find some meaning in my life as you play.
     
     Tips: This game only works on browser. Press F11 to go into fullscreen for the full immersive experience. Refresh the page to restart 😄
-
     Enjoy the time you're about to spend with me ❤️
-
      
      [NOTE: This game is incomplete. For the first half of the game, only the left option works.]
     `,
@@ -87,7 +85,6 @@ const textNodes = [
   {
     id: 2,
     text: `In life, we are given lemons. That's just part of what life is. And lemons suck, they suggest sourness and difficulty. They are anxiety-inducing. They are the result of disappointment and failure.
-
     Although the hardships may be out of our control, in the face of adversity we are given the choice between two options.
     
     When life gives you lemons, what do you do?`,
@@ -155,7 +152,6 @@ const textNodes = [
   {
     id: 5,
     text: `OKAYYY! 😊 I knew you'd say this. I love lemonade. Sometimes I wish I could just float in a pool of lemonade. Is that weird? OMG and I also love Gong Cha. Most notably White pearl & Ai-Yu Special or Honey Lemon drink (0% ice and 0% sugar, of course).
-
     Anyways... maybe next time I see you on campus... you can buy me some bubble tea? HAHA jk! unless...?`,
     options: [
       {
@@ -171,7 +167,6 @@ const textNodes = [
   {
     id: 6,
     text: `Yo that's lit HAHA. I didn't know you liked drinking... 😅
-
     (Lemon Boy looks nervous. Because of his introverted nature, he isn't the best at carrying on conversations.)`,
     options: [
       {
@@ -218,7 +213,6 @@ const textNodes = [
   {
     id: 8,
     text: `Lemon Boy does not seem amused. He has heard this a million times before, and he doesn't like being associated with such a peasant game.
-
     He likes to believe he is so much more than a video game (He is not)`,
     options: [
       {
@@ -230,7 +224,6 @@ const textNodes = [
   {
     id: 9,
     text: `Well what did you expect? I'm a bloody lemon. Haha. It's not like I grew on trees. Wait, was I suppose to? Crap.
-
     I hope you can still accept me for who I am despite being a weed lemon 😕`,
     options: [
       {
@@ -290,7 +283,6 @@ const textNodes = [
   {
     id: 13,
     text: `Lemon Boy rarely even receives compliments; even fewer from members of the opposite gender. Asking him on a date causes an unstable reaction within his biological make up.
-
     Lemon Boy suddenly struggles to keep eye contact. His face flushes nervously and lights up like a tomato. You become worried as he is seemingly paralyzed.`,
     options: [{
       text: `Are you okay?`,
@@ -320,7 +312,6 @@ const textNodes = [
   {
     id: 15,
     text: `OHHH uhh... sorry😅. I auto piloted for a second. What did you say?
-
     OH YEAH. A date. UHHHHHHHHH. Sure! haha. Yea. I get totally asked out on a lot of dates, so this is like nothing special just FYI. But I'll accept this anyways. Of course I'll go on a date with you.
     
     😳Why am I blushing? I AM?! OH uhhhh... Aw geez its hot today huh...
@@ -359,8 +350,6 @@ const textNodes = [
   }, {
     id: 17,
     text: `Lemon Boy becomes filled with DETERMINATION ❤️
-
-
     Lemon Boy realizes he is strong and courageous. He approaches the big bug with love and compassion, requesting for it to move away. Miraculously, the bug notices his demeanor and complies.`,
     options: [{
       text: `Hurry up you're late`,
@@ -370,7 +359,6 @@ const textNodes = [
   }, {
     id: 18,
     text: `A bug is more important than me? IS THAT WHAT YOU'RE SAYING??? You unfold into an aquatic beast. Lemon Boy's lateness fills you with disdain. You scream, causing Lemon Nation to shake frantically beneath your feet.
-
     Tsunamis surge up and sweep the land. Within minutes, every inhabitable corner turns into a graveyard. The water bleeds through dumps of debris and dead lemons.
     
     Worst of all, it's king has fallen. Lemon Boy's body leaves the earth but Lemonlord's legacy will remain forever.`,
@@ -382,7 +370,6 @@ const textNodes = [
   }, {
     id: 19,
     text: `...Or so you though!
-
     Lemon Boy rises from the ashes. The scar on his face only heightens his valiant pose. A patch of light seeps through the sky and spotlights his prowess.
     
     "Shall we continue with our date😉?" He smirks. Lemon Boy has never been more smooth in his life. You feel something off with him. Something is definitely wrong...`,
@@ -394,7 +381,6 @@ const textNodes = [
   }, {
     id: 20,
     text: `The sunset blossoms red and gold, flourishing the romantic mood.
-
     Hey gurl, are you honey? because you can be the honey to my lemon. 😎
     
     [The first thing that comes out of his mouth, Lemon Boy attempts a very cheesy pick up line. He is the opposite of smooth.]`,
@@ -440,7 +426,6 @@ const textNodes = [
   }, {
     id: 22,
     text: `🍋 MMmmm good question. I feel obligated to say lemons, but I really like munching at blueberries. Peaches are nice too. The crunchy ones, not the soft ones! I also like watermelons, pomegranates, zucchini... man there's too many 🍉.
-
     Weird question. Do you enjoy going to the aquarium?`,
     options: [{
       text: `Yep I love the aquarium`,
@@ -465,7 +450,6 @@ const textNodes = [
   }, {
     id: 23,
     text: `OOOohh. I love the aquarium. Sometimes I wish I can be breathe underwater and swim among the fishes.
-
     Let's talk about dating. What's more important to you, personality or looks?`,
     options: [{
       text: `Personality. Their heart reveals everything`,
@@ -486,15 +470,14 @@ const textNodes = [
   }, {
     id: 24,
     text: `Yooo good answer, I agree! The reflected behaviours and attitude of a person is so much more important.
-
-    By the way, Are you a Christian?`,
+    Do you like lemons?`,
     options: [{
-      text: `No, I'm not a Christian
+      text: `No, I don't like lemons
       [UNFINISHED TREE]`,
       nextText: 24
     },
     {
-      text: `Yes, I'm a Christian`,
+      text: `Yes, I love lemons`,
       nextText: 27
     }, {
       text: `I don't care. Why did you make this game, "Lemon Boy"?`,
@@ -509,7 +492,6 @@ const textNodes = [
   {
     id: 25,
     text: `But I look like a blobfish 😭. Oh well.
-
     I've always wondered, are you a Christian?`,
     options: [{
       text: `No, I'm not a Christian`,
@@ -530,7 +512,6 @@ const textNodes = [
   }, {
     id: 27,
     text: `Yoooo that's lit!
-
     `,
     options: [{
       text: `Why did you make this game, Lemon Boy?`,
@@ -551,7 +532,6 @@ const textNodes = [
   }, {
     id: 29,
     text: `PLEASE refrain from breaking the fourth wall in the future. Too much of it will shatter the game and cause weird things to happen...
-
     Lets rewind shall we. I'll pretend that never happened.`,
     options: [{
       text: `Continue`,
@@ -561,7 +541,6 @@ const textNodes = [
   }, {
     id: 32,
     text: `HELLO?? Are you even listening to me? I'm starting to get a little frustrated. It seems like you can't even follow simple instructions. Do I need to restrict you like a child? This is my world. Please follow my rules. I am trying to protect you.
-
     Do not break the fourth wall.`,
     options: [{
       text: `I don't care. Let me out of this game`,
@@ -601,7 +580,6 @@ const textNodes = [
   }, {
     id: 34,
     text: `I AM LETTING YOU LIVE INSIDE THIS UNIVERISE OF BLISS AND CONTENT.
-
     EAT THE DAMN BLUE PILL AND BE HAPPY WITH IT GOSH DARNIT.
     
     THE WORLD OUT THERE IS CRUEL. LIFE IS UNFAIR. WHAT DON'T YOU UNDERSTAND ABOUT THAT? I'M LOSING MY PATIENCE.`,
@@ -625,7 +603,6 @@ const textNodes = [
   }, {
     id: 36,
     text: `It's a beautiful day outside. Birds are singing. Flowers are blooming.
-
     On days like these...
    .
    .
@@ -783,7 +760,6 @@ const textNodes = [
   }, {
     id: 54,
     text: `Lemon Boy falls off the shore and plummets into the ocean. He doesn't know how to swim. He begins to splash and drown.
-
     A look of horror and regret on his face.
     
     "HELPPP!" he screams.`,
@@ -798,7 +774,6 @@ const textNodes = [
   },{
     id: 55,
     text: `You jump in the water with Lemon Boy, attempting to keep him afloat. You realize your mistake. Lemon Boy begins to pull and grab at you. You can't communicate to him that he's pulling you down with him. It's his natural instincts to want to survive.
-
     No matter how much you struggle, Lemon Boy tugs at your limbs. You both begin to drown.`,
     options: [{
       text: `.`,
@@ -808,7 +783,6 @@ const textNodes = [
   }, {
     id: 71,
     text: `What do you see?
-
     DEATH
     DEATH
     DEATH
@@ -963,12 +937,8 @@ const textNodes = [
     text: `[You start to realize it now.]
     
     Lemon Boy is cancer. Everyone he touches gets hurt. Everyone who has attempted to truly get to know him, and attempt to save him gets dragged down into his own incurable nightmare.
-
 Yet still...
-
 you chose love... 
-
-
 why?
     `,
     options: [{
@@ -981,7 +951,6 @@ why?
     text: `[You start to realize it now.]
     
     Lemon Boy is cancer. Everyone he touches gets hurt. Everyone who has attempted to truly get to know him, and attempt to save him gets dragged down into his own incurable nightmare.
-
     You saw death, and death was the answer all along. 
     `,
     options: [{
@@ -992,7 +961,6 @@ why?
   }, {
     id: 66,
     text: `❤️Thanks for playing! The stuff in this game are imaginative. this is a work of fiction. None of what's in this game is true or a reflection of anyone I know.
-
     I was originally meaning for this game to way longer, with storylines and multiple endings, but the game was taking way too long and the code was getting way to complicated. So it's like 33% finished. I hope you enjoyed it anyways. If you are feeling generous, click on the little drawing of Lemon Boy you see.
     `,
     options: [{
